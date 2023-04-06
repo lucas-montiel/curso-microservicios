a897050833a55000608f600e39542a852b26c260
lucas-montiel <lucas.gabriel.m.2696@gmail.com>
2023-04-06 15:45:56 -0300

    Se agrego la eliminacion de un usuario y que al mismo tiempo se desasigne del curso

--- msvc-usuarios/src/main/java/com/springcloud/msv/usuarios/microservicios/client/CursoClientRest.java
+++ msvc-usuarios/src/main/java/com/springcloud/msv/usuarios/microservicios/client/CursoClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="msvc-curso", url="8002")
+@FeignClient(name="msvc-cursos", url="localhost:8002")
 public interface CursoClientRest {
     @DeleteMapping("curso/desasignar-usuario/{id}")
     void desasignarUsuarioCurso(@PathVariable Long id);
